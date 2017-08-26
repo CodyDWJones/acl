4bd3cb03367e246c94b110f90b7dedbba87dbed8
Cody Jones <cody.dw.jones@gmail.com>
2017-08-26 09:33:03 -0700

    Restoring a conditional I shouldn't have removed.

--- includes/acl/compression/stream/write_stream_data.h
+++ includes/acl/compression/stream/write_stream_data.h
@@ -221,8 +221,6 @@ namespace acl
 			uint8_t bit_rate = track_stream.get_bit_rate();
 			uint64_t num_bits_at_bit_rate = get_num_bits_at_bit_rate(bit_rate) * 3;	// 3 components
 
-			// TODO: I broke something here, but don't understand yet why.
-
 			// Track is constant, our constant sample is stored in the range information
 			ACL_ENSURE(!is_pack_0_bit_rate(bit_rate), "Cannot write constant variable track data");
 			const uint8_t* raw_sample_ptr = track_stream.get_raw_sample_ptr(sample_index);
@@ -290,10 +288,10 @@ namespace acl
 			{
 				const BoneStreams& bone_stream = bone_streams[bone_index];
 
-				if (bone_stream.is_rotation_animated())
+				if (bone_stream.is_rotation_animated() && !is_pack_0_bit_rate(bone_stream.rotations.get_bit_rate()))
 					write_animated_track_data(bone_stream.rotations, sample_index, has_mixed_packing, animated_track_data_begin, animated_track_data, bit_offset);
 
-				if (bone_stream.is_translation_animated())
+				if (bone_stream.is_translation_animated() && !is_pack_0_bit_rate(bone_stream.translations.get_bit_rate()))
 					write_animated_track_data(bone_stream.translations, sample_index, has_mixed_packing, animated_track_data_begin, animated_track_data, bit_offset);
 
 				ACL_ENSURE(animated_track_data <= animated_track_data_end, "Invalid animated track data offset. Wrote too much data.");
